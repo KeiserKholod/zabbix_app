f2e19ddde6d64249fbd11e5401979254345fe584
Dimitry <Kholod.ural@gmail.com>
2023-04-16 13:07:29 +0500

    created module to work with git

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import os
+import subprocess
 import sys
 
 import zabbix_app.app_cli as app_cli
@@ -19,6 +20,30 @@ def _set_conf_process(i, part, all_args):
     cs = ZabbixConfigSetter(part, all_args)
     cs.upd_config_for_all()
     print("Set Successful; Process: " + str(i))
+
+
+def call_git(path_to_dir):
+    current_dir = os.getcwd()
+
+    os.chdir(path_to_dir)
+    answ = subprocess.run(["git", "status"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).__str__()
+    if answ.find("not a git repository") > -1:
+        print("GIT: Creating git-repo")  # Hello from the other side
+        answ = subprocess.run(["git", "init"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).__str__()
+        if answ.find("initialized") > -1:
+            print("GIT: Repo initialized in " + path_to_dir)
+
+    answ = subprocess.run(["git", "status"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).__str__()
+    if answ.find("untracked files present") > -1 or answ.find("Changes not staged for commit") > -1:
+        answ = subprocess.run(["git", "add", "*"], stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT).__str__()
+        answ = subprocess.run(["git", "commit", "-m", '"message"'], stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT).__str__()
+        print("GIT: Commit created")
+    else:
+        print("GIT: nothing to commit")
+
+    os.chdir(current_dir)
 
 
 if __name__ == '__main__':
@@ -51,7 +76,13 @@ if __name__ == '__main__':
         all_hosts = zabbix_connection.get_host_list()
         parts = zabbix_connection.get_part_hosts(all_hosts, threads_count)
         i = 0
+        process = []
         for part in parts:
             p = multiprocessing.Process(target=_get_conf_process, args=[i, part, all_args])
             p.start()
+            process.append(p)
             i += 1
+        # синхронизация с main процессом
+        for p in process:
+            p.join()
+        call_git(all_args["saving_dir"])
