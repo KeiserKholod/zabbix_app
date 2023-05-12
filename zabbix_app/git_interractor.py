96221e3ac1adde6c59f2e343faf8b88446b4c658
Dimitry <Kholod.ural@gmail.com>
2023-05-13 00:29:22 +0500

    tst

--- zabbix_app/git_interractor.py
+++ zabbix_app/git_interractor.py
@@ -16,18 +16,18 @@ class GitInterractor:
 
         os.chdir(path_to_dir)
         answ = subprocess.run(["git", "status"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).__str__()
-        print(answ)
+        print(answ.encode("unicode"))
         if answ.find("not a git repository") > -1 or answ.find("Не найден") > -1:
             app_cli.write_log_file(all_args["log"], "GIT: Creating git-repo")
             answ = subprocess.run(["git", "init"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).__str__()
-            print(answ)
+            print(answ.encode("unicode"))
             if answ.find("initialized") > -1 or answ.find("Инициализирован") > -1:
                 os.chdir(current_dir)
                 app_cli.write_log_file(all_args["log"], "GIT: Repo initialized in " + path_to_dir)
                 os.chdir(path_to_dir)
 
         answ = subprocess.run(["git", "status"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).__str__()
-        print(answ)
+        print(answ.encode("unicode"))
         if answ.find("untracked files present")  > -1 or answ.find("неотслеживаемые файлы") > -1 or answ.find("Changes not staged for commit") > -1 or answ.find("ничего не добавлено") > -1:
             answ = subprocess.run(["git", "add", "*"], stdout=subprocess.PIPE,
                                   stderr=subprocess.STDOUT).__str__()
