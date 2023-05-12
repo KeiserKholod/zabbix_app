e2177f996b1e48cecd2e0332bb49e154d177e24d
Dimitry <Kholod.ural@gmail.com>
2023-05-13 00:43:14 +0500

    addd russian lang

--- zabbix_app/git_interractor.py
+++ zabbix_app/git_interractor.py
@@ -18,12 +18,12 @@ class GitInterractor:
         answ = subprocess.run(["git", "status"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         answ = answ.stdout.decode("utf-8")
         answ = answ.__str__()
-        if answ.find("not a git repository") > -1:
+        if answ.find("not a git repository") > -1 or answ.find("Не найден") > -1:
             app_cli.write_log_file(all_args["log"], "GIT: Creating git-repo")
             answ = subprocess.run(["git", "init"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             answ = answ.stdout.decode("utf-8")
             answ = answ.__str__()
-            if answ.find("initialized") > -1:
+            if answ.find("initialized" or answ.find("Инициализирован") > -1) > -1:
                 os.chdir(current_dir)
                 app_cli.write_log_file(all_args["log"], "GIT: Repo initialized in " + path_to_dir)
                 os.chdir(path_to_dir)
@@ -31,7 +31,8 @@ class GitInterractor:
         answ = subprocess.run(["git", "status"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         answ = answ.stdout.decode("utf-8")
         answ = answ.__str__()
-        if answ.find("untracked files present") > -1 or answ.find("Changes not staged for commit") > -1:
+        if answ.find("untracked files present") > -1 or answ.find("Неотслеживаемые файлы") > -1 or answ.find(
+                "Changes not staged for commit") > -1 or answ.find("ничего не добавлено") > -1:
             answ = subprocess.run(["git", "add", "*"], stdout=subprocess.PIPE,
                                   stderr=subprocess.STDOUT)
             answ = answ.stdout.decode("utf-8")
