c668aa270b9216bf87cc3b96a9b4249dd3a4806d
KeiserKholod <38317130+KeiserKholod@users.noreply.github.com>
2023-05-13 00:31:55 +0500

    Update git_interractor.py

--- zabbix_app/git_interractor.py
+++ zabbix_app/git_interractor.py
@@ -25,7 +25,7 @@ class GitInterractor:
                 os.chdir(path_to_dir)
 
         answ = subprocess.run(["git", "status"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).__str__()
-        if answ.find("untracked files present")  > -1 or answ.find("Неотслеживаемые файлы") > -1 or answ.find("Changes not staged for commit") > -1 or answ.find("ничего не добавлено") > -1:
+        if answ.find("untracked files present")  > -1 or answ.find("неотслеживаемые файлы") > -1 or answ.find("Changes not staged for commit") > -1 or answ.find("ничего не добавлено") > -1:
             answ = subprocess.run(["git", "add", "*"], stdout=subprocess.PIPE,
                                   stderr=subprocess.STDOUT).__str__()
             answ = subprocess.run(["git", "commit", "-m", '"message"'], stdout=subprocess.PIPE,
