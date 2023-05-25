fc5971ea59b69bb3e3b1d056bdc65fb84bbc0f46
KeiserKholod <38317130+KeiserKholod@users.noreply.github.com>
2023-05-25 09:48:13 +0500

    change output strings from console to lowercase

--- zabbix_app/git_interractor.py
+++ zabbix_app/git_interractor.py
@@ -17,31 +17,31 @@ class GitInterractor:
         os.chdir(path_to_dir)
         answ = subprocess.run(["git", "status"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         answ = answ.stdout.decode("utf-8")
-        answ = answ.__str__()
-        if answ.find("not a git repository") > -1 or answ.find("Не найден") > -1:
+        answ = answ.__str__().lower()
+        if answ.find("not a git repository") > -1 or answ.find("не найден") > -1:
             app_cli.write_log_file(all_args["log"], "GIT: Creating git-repo")
             answ = subprocess.run(["git", "init"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             answ = answ.stdout.decode("utf-8")
-            answ = answ.__str__()
+            answ = answ.__str__().lower()
 
-            if answ.find("initialized") > -1 or answ.find("Инициализирован") > -1 or answ.find("инициализирован") > -1:
+            if answ.find("initialized") > -1 or answ.find("инициализирован") > -1:
                 os.chdir(current_dir)
                 app_cli.write_log_file(all_args["log"], "GIT: Repo initialized in " + path_to_dir)
                 os.chdir(path_to_dir)
 
         answ = subprocess.run(["git", "status"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         answ = answ.stdout.decode("utf-8")
-        answ = answ.__str__()
-        if answ.find("untracked files present") > -1 or answ.find("Неотслеживаемые файлы") > -1 or answ.find(
-                "Changes not staged for commit") > -1:
+        answ = answ.__str__().lower()
+        if answ.find("untracked files present") > -1 or answ.find("неотслеживаемые файлы") > -1 or answ.find(
+                "changes not staged for commit") > -1:
             answ = subprocess.run(["git", "add", "*"], stdout=subprocess.PIPE,
                                   stderr=subprocess.STDOUT)
             answ = answ.stdout.decode("utf-8")
-            answ = answ.__str__()
+            answ = answ.__str__().lower()
             answ = subprocess.run(["git", "commit", "-m", '"message"'], stdout=subprocess.PIPE,
                                   stderr=subprocess.STDOUT)
             answ = answ.stdout.decode("utf-8")
-            answ = answ.__str__()
+            answ = answ.__str__().lower()
             os.chdir(current_dir)
             app_cli.write_log_file(all_args["log"], "GIT: Commit created")
             os.chdir(path_to_dir)
